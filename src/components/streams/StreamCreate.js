248baf8a195fd0703e77584d7125a30e0d661364
Carrie Mathieu <carrieburks@gmail.com>
2021-05-06 22:54:58 -0400

    added onSubmit event listener

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,15 +2,28 @@ import React from 'react'
 import { Field, reduxForm } from 'redux-from'
 
 class StreamCreate extends React.Component {
-    renderInput({ input }) {
-        return <input {...input}/>
+    // accepts label as arg, assigns to each input field
+    renderInput({ input, label }) {
+        return (
+            <div className="field">
+                <label>{label}</label>
+                <input {...input}/>
+            </div>
+        )
     }
 
+    // formProps -> incl w/ redux-form
+    onSubmit(formValues) {
+        console.log(formValues)
+    }
+
+    // passes label as prop to renderInput function
     render() {
         return (
-            <form>
-                <Field name="title" component={this.renderInput}/>
-                <Field name="description" component={this.renderInput}/>
+            <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+                <Field name="title" component={this.renderInput} label="Enter Title"/>
+                <Field name="description" component={this.renderInput} label="Enter Description"/>
+                <button className="ui button primary">submit</button>
             </form>
         )
     }
